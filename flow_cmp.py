730a3c0f9664b7c7c0ba8fa60fb7f7f29fc46e10
Christoph Loibl <20111535+stoffi92@users.noreply.github.com>
2017-10-19 20:55:53 +0200

    removed comments that were copied from the draft

--- flow_cmp.py
+++ flow_cmp.py
@@ -49,26 +49,21 @@ def flow_rule_cmp(a, b):
     """
     Implementation of the flowspec sorting algorithm in draft-ietf-idr-rfc5575bis-04.
     """
-    for comp_a, comp_b in itertools.zip_longest(a.components, b.components):
-        # If the types differ, the rule with lowest numeric type value has
-        # higher precedence (and thus will match before) than the rule that
-        # doesn't contain that component type.
+    for comp_a, comp_b in itertools.zip_longest(a.components,
+                                           b.components):
+        # If a component type does not exist in one rule
+        # this rule has lower precedence
         if not comp_a:
             return B_HAS_PRECEDENCE
         if not comp_b:
             return A_HAS_PRECEDENCE
+        # Higher precedence for lower component type
         if comp_a.component_type < comp_b.component_type:
             return A_HAS_PRECEDENCE
         if comp_a.component_type > comp_b.component_type:
             return B_HAS_PRECEDENCE
-        # If the component types are the same, then a type- specific comparison
-        # is performed.
+        # component types are equal -> type specific comparison
         if comp_a.component_type in (IP_DESTINATION, IP_SOURCE):
-            # For IP prefix values (IP destination and source prefix) precedence
-            # is given to the lowest IP value of the common prefix length; if
-            # the common prefix is equal, then the most specific prefix has
-            # precedence.
-
             # assuming comp_a.value, comp_b.value of type ipaddress
             if comp_a.value.overlaps(comp_b.value):
                 # longest prefixlen has precedence
@@ -76,38 +71,26 @@ def flow_rule_cmp(a, b):
                     return A_HAS_PRECEDENCE
                 if comp_a.value.prefixlen < comp_b.value.prefixlen:
                     return B_HAS_PRECEDENCE
-                # otherwise supernet the ip component is equal -
-                # continue with next component
+                # components equal -> continue with next component
             elif comp_a.value > comp_b.value:
                 return B_HAS_PRECEDENCE
             elif comp_a.value < comp_b.value:
                 return A_HAS_PRECEDENCE
-            # equal value cannot happen here since this is covered
-            # in the branch(1) above
         else:
-            # For all other component types, unless otherwise specified, the
-            # comparison is performed by comparing the component data as a
-            # binary string using the memcmp() function as defined by the ISO C
-            # standard. For strings of different lengths, the common prefix is
-            # compared.  If equal, the longest string is considered to have
-            # higher precedence than the shorter one.
-
             # assuming comp_a.value, comp_b.value of type bytearray
             if len(comp_a.value) == len(comp_b.value):
                 if comp_a.value > comp_b.value:
                     return B_HAS_PRECEDENCE
                 if comp_a.value < comp_b.value:
                     return A_HAS_PRECEDENCE
-                # otherwise this component is equal -
-                # continue with next component
+                # components equal -> continue with next component
             else:
                 common = min(len(comp_a.value), len(comp_b.value))
                 if comp_a.value[:common] > comp_b.value[:common]:
                     return B_HAS_PRECEDENCE
                 elif comp_a.value[:common] < comp_b.value[:common]:
                     return A_HAS_PRECEDENCE
-                # the first common characters match, the longer string
-                # has precedence
+                # the first common bytes match
                 elif len(comp_a.value) > len(comp_b.value):
                     return A_HAS_PRECEDENCE
                 else:
